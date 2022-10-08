27d094e7ac74bb0967e12988fefb81d841a0e1ab
muse_stargazer <bertadams00@gmail.com>
2022-10-08 06:03:34 +0000

    added copied ajax code to insert list files, added setTimeout function to the JS-added list items so they'd appear at the bottom

--- scripts.js
+++ scripts.js
@@ -1,7 +1,39 @@
 // JS for TP5
 
 
+//funtion to load a file from the url "fromFile" into the object identified as "whereTo"
+function loadFileInto(fromFile, whereTo) {
+
+  // creating a new XMLHttpRequest object
+  ajax = new XMLHttpRequest();
+
+  // defines the GET/POST method, source, and async value of the AJAX object
+  ajax.open("GET", fromFile, true);
+
+  // provides code to do something in response to the AJAX request
+  ajax.onreadystatechange = function() {
+    if ((this.readyState == 4) && (this.status == 200)) {
+      document.querySelector(whereTo).innerHTML += this.responseText;
+
+    } else if ((this.readyState == 4) && (this.status != 200)) {
+      console.log("Error: " + this.responseText);
+    }
+
+  } // end ajax.onreadystatechange function
+
+  // initiate request and wait for response
+  ajax.send();
+
+}
+
+
 window.onload = function() {
+
+  loadFileInto("ingredients.html", "#ingredients ul");
+
+  loadFileInto("equipment.html", "#equipment ul");
+
+  loadFileInto("directions.html", "#directions ol");
 
   document.querySelector("#header h1").classList.add("tp");
 
@@ -29,11 +61,14 @@ window.onload = function() {
 
   document.querySelector("#footer").classList.add("bg");
 
-  document.querySelector("#bigflex #ingredients ul").innerHTML += '<li>A smattering of <span class="notice">sunshine dust</span></li>';
+  setTimeout(function() {
+    document.querySelector("#bigflex #ingredients ul").innerHTML += '<li>A smattering of <span class="notice">sunshine dust</span></li>';
 
-  document.querySelector("#bigflex #equipment ul").innerHTML += '<li><span class="notice"><span class="measure">36-inch</span> claw of a dragon</span> <span class="measure">(Species up to personal preference)</span></li>';
+    document.querySelector("#bigflex #equipment ul").innerHTML += '<li><span class="notice"><span class="measure">36-inch</span> claw of a dragon</span> <span class="measure">(Species up to personal preference)</span></li>';
 
-  document.querySelector("#bigflex #directions ol").innerHTML += '<li>Use <span class="notice">dragon claw</span> to shave <span class="notice">sunshine dust</span> <span class="measure">sparingly</span> over flan.</li><li>Enjoy!</li>';
+    document.querySelector("#bigflex #directions ol").innerHTML += '<li>Use <span class="notice">dragon claw</span> to shave <span class="notice">sunshine dust</span> <span class="measure">sparingly</span> over flan.</li><li>Enjoy!</li>';
+  }, 100);
+
 
 
 }; // end window.onload
